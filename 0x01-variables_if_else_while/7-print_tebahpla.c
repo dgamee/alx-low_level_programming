abe87a588704676284693f5cc56b8d8ccadae532
Ghosthands <rayden.ai@gmail.com>
2023-06-18 23:36:53 +0100

    1 to 8 solution

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Print the alphabet in lowercase letters in reverse
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'z'; letter >= 'a'; letter--)
+	putchar(letter);
+	putchar('\n');
+
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Print the numbers of base 16 in lowercase
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	char digit;
+
+	for (digit = '0'; digit <= '9'; digit++)
+	putchar(digit);
+
+	for (digit = 'a'; digit <= 'f'; digit++)
+	putchar(digit);
+	putchar('\n');
+
+	return (0);
+}
