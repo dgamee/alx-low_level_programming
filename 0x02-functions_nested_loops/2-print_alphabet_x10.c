9dc6cf0e8ac39e9c06171b814c1b3a0b4b495bc7
Ghosthands <rayden.ai@gmail.com>
2023-06-21 09:57:21 +0100

    fix

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -14,10 +14,12 @@ void print_alphabet_x10(void)
 
 	while (i < 10)
 	{
-		for (letter = 'a'; letter <= 'z'; letter ++)
+		for (letter = 'a'; letter <= 'z'; letter++)
 		{
 			_putchar(letter);
 		}
 		_putchar('\n');
+		i++;
 	}
+	return (0);
 }
