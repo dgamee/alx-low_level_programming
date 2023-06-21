51875c894385653bc62e58842cc0866f6725ad9f
Ghosthands <rayden.ai@gmail.com>
2023-06-21 09:48:36 +0100

    print 10 times the alphabet in lowercase follwed by a new line

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print 10 times the alphabet in lowercase
+ *
+ * Return: 0 (sucess)
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	char letter;
+
+	while (i < 10)
+	{
+		for (letter = 'a'; letter <= 'z; letter ++)
+		{
+			_putchar(letter);
+		}
+		_putchar('\n');
+	}
+}
