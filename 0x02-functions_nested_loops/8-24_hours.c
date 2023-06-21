afe8e70813e519af40f6935e3373352d2dc1890a
Ghosthands <rayden.ai@gmail.com>
2023-06-21 14:05:11 +0100

    function that print every minutes of the day starting from 00:00 to 23:59

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * jack_bauer - print time in 24 hours format
+ *
+ */
+
+void jack_bauer(void)
+{
+	int hr, min;
+
+	for (hr = 00; hr < 24; hr++)
+	{
+		for (min = 00; min < 60; min++)
+		{
+			if (hr < 10 && min < 10)
+				printf("0%d:0%d\n", hr, min);
+			else if (hr < 10)
+				printf("0%d:%d\n", hr, min);
+			else if (min < 10)
+				printf("%d:0%d\n", hr, min);
+			else
+				printf("%d:%d\n", hr, min);
+		}
+	}
+}
