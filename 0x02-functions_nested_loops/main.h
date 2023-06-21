b012bd2600cdba0483e6a281ee8a7bfb94134ed7
Ghosthands <rayden.ai@gmail.com>
2023-06-21 07:23:07 +0100

    hot fix

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,9 @@
+int _putchar(char c);
+int print_alphabet(void);
+int print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int i);
+int print_last_digit(int i);
+void jack_bauer(void);
