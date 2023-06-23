8779c974db400545e509b2b73132d589150028eb
Ghosthands <rayden.ai@gmail.com>
2023-06-23 10:35:51 +0100

    function that checks for a digit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -12,16 +12,9 @@ int _isdigit(int c)
 {
 	int value;
 
-	if (c >= 0 && c <= 9)
+	if (c >= '0' && c <= '9')
 	{
-		if (isdigit(c))
-		{
-			value = 1;
-		}
-		else
-		{
-			value = 0;
-		}
+		value = 1;
 	}
 	else
 	{
