f3f35ce2020cc2160d70dd526ea03d517fe37847
Ghosthands <rayden.ai@gmail.com>
2023-06-27 15:38:33 +0100

    task

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string followed by a new line
+ * @str: word to be printed in half
+ *
+ */
+
+void puts_half(char *str)
+{
+	int length = strlen(str);
+	char word;
+	int i, n;
+
+	if (length % 2 == 0)
+	{
+		n = (length - 1) / 2;
+		for (i = n + 1; i < length; i++)
+		{
+			word = str[i];
+			putchar(word);
+		}
+	}
+	else
+	{
+		n = (length - 1) / 2;
+		for (i = n + 2; i < length; i++)
+		{
+			word = str[i];
+			putchar(word);
+		}
+	}
+	putchar('\n');
+}
