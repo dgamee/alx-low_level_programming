1cc4e7a0201ba6de863d4412083517ecce8f5100
Ghosthands <rayden.ai@gmail.com>
2023-06-27 13:33:44 +0100

    initial

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+ * print_rev - print a string in reverse follwed by a new line
+ * @s: param of character type
+ *
+ */
+
+void rev_string(char *s)
+{
+	int strlength = strlen(s);
+	int i;
+
+	for (i = strlength; i > 0; i--)
+	{
+		int cat = i - 1;
+
+		putchar(s[cat]);
+	}
+	putchar('\n');
+}
