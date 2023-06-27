5e8c35385c1d1d0fedf61d2e5093e36cff9b1418
Ghosthands <rayden.ai@gmail.com>
2023-06-27 12:43:28 +0100

    fix variable name

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,15 +10,15 @@
 void print_rev(char *s)
 {
 	int strlength = strlen(s);
-	char strreversed[strlength + 1];
+	char reversed[strlength + 1];
 	int i = 0;
 
 	while (strlength > 0)
 	{
-		strreversed[i] = s[strlength - 1];
+		reversed[i] = s[strlength - 1];
 		strlength--;
 		i++;
 	}
-	strreversed[i] = '\0';
-	printf("%s\n", strreversed);
+	reversed[i] = '\0';
+	printf("%s\n", reversed);
 }
