eba0422df2ef75ca2c547330b6d90cb771df68f2
Ghosthands <rayden.ai@gmail.com>
2023-06-29 14:22:55 +0100

    task 2

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strncat - functions that concatenate two strings
+ * @dest: pointer of typer char
+ * @src: pointer of type char
+ * @n: most bytes from src
+ *
+ * Return: returns a string
+ *
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int dest_len = strlen(dest);
+	int i = 0;
+
+	while (i < n && src[i] != '\0')
+	{
+		dest[dest_len + i] = src[i];
+	}
+	dest[dest_leen + i] = '\0';
+	return (dest);
+}
