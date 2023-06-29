a56b38eede78037957da9c41952bfc567cbfeede
Ghosthands <rayden.ai@gmail.com>
2023-06-29 12:38:01 +0100

    task 1

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strcat - functions that concatenate two strings
+ * @dest: pointer of typer char
+ * @src: pointer of type char
+ *
+ * Return: returns a string
+ *
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	char result;
+
+	result = strcat(dest, src);
+	return (result);
+}
