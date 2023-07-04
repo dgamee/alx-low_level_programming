7b9234b35815b7ab1515d456e5ff8aa64e3a3f71
rayden <rayden.ai@gmail.com>
2023-07-04 12:51:41 +0100

    fixed my coding style

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,10 +2,11 @@
 #include "main.h"
 
 /**
-  * memset - function that fills memory with a constant byte
+  * _memset - function that fills memory with a constant byte
   * @s: pointer of type char
   * @b: param of char datatype
   * @n: int datatype
+  * Return: Returns its first argument
   */
 
 char *_memset(char *s, char b, unsigned int n)
