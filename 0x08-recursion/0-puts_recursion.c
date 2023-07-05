a020911fee9d83c10f7dd38bb129782cd32e57fe
rayden <rayden.ai@gmail.com>
2023-07-05 22:50:19 +0100

    initial commit

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include <string.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts_recursion - A function that prints string followed by a new line
+ * @s: string pointer
+ */
+
+void _puts_recursion(char *s)
+{
+	printf("%s\n", s);
+}
