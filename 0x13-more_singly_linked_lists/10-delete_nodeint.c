593c70f50b9261fb7e8f03bf6197c91531fa46fc
rayden <rayden.ai@gmail.com>
2023-08-23 11:23:38 +0100

    bug fix for 10...

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -19,20 +19,22 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		return (-1);
 	if (index == 0)
 	{
-		*head = (*head) ->next;
+		*head = (*head)->next;
 		free(temp);
 		return (1);
 	}
+
 	while (i < index - 1)
 	{
-		if (!temp || !(temp ->next))
+		if (!temp || !(temp->next))
 			return (-1);
-		temp = temp ->next;
+		temp = temp->next;
 		i++;
 	}
 
-	current = temp ->next;
+	current = temp->next;
 	temp->next = current->next;
 	free(current);
+
 	return (1);
 }
