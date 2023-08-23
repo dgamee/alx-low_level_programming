76cbcaee3e06c07eaea7c76a214d80675d8eef77
rayden <rayden.ai@gmail.com>
2023-08-23 11:08:41 +0100

    bug fix for 0-print..

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,24 +9,14 @@
 */
 size_t print_listint(const listint_t *h)
 {
-	size_t node_count = 1;
+	size_t num = 0;
 
-	/* return 0 as no of nodes when h is null*/
-	if (h == NULL)
-		return (0);
-
-	while (h->next != NULL)
+	while (h)
 	{
 		printf("%d\n", h->n);
-
-		/*go to the next node*/
+		num++;
 		h = h->next;
-		/*count no of nodes*/
-		node_count++;
 	}
 
-	/*print last node*/
-	printf("%d\n", h->n);
-
-	return (node_count);
+	return (num);
 }
